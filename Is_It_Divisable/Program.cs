a2ec4dc7a3c40309bb44822b8cecbf8c78b85afa
joshuapickenpaugh <joshuapickenpaugh@hotmail.com>
2017-02-27 10:36:10 -0600

    Add project files.

---
+++ Is_It_Divisable/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Is_It_Divisable
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
